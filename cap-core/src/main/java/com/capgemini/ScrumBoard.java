d25f6346fb25d16b0a5ed8172f3402629936fe69
Sanjin Cevro <scevro@.corp.capgemini.com>
2011-06-06 13:02:16 +0200

    Added new class

---
+++ cap-core/src/main/java/com/capgemini/ScrumBoard.java
@@ -0,0 +1,16 @@
+package com.capgemini;
+
+import java.io.Serializable;
+
+/**
+ * @author scevro
+ * 
+ */
+public class ScrumBoard implements Serializable {
+
+	/**
+	 * Serial version UID for class.
+	 */
+	private static final long serialVersionUID = -6243959646151989431L;
+
+}
