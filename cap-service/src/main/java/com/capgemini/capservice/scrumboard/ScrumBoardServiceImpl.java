16f0e100b77f4c9920b79156268bea13cffd4a9a
Sanjin Cevro <sanjin.cevro@capgemini.com>
2011-08-03 12:11:06 +0200

    Added author tag

--- cap-service/src/main/java/com/capgemini/capservice/scrumboard/ScrumBoardServiceImpl.java
+++ cap-service/src/main/java/com/capgemini/capservice/scrumboard/ScrumBoardServiceImpl.java
@@ -3,6 +3,11 @@ package com.capgemini.capservice.scrumboard;
 import com.capgemini.core.domain.ScrumBoard;
 import com.capgemini.persistence.dao.ScrumBoardDao;
 
+/**
+ * 
+ * @author Sanjin Cevro (Capgemini)
+ *
+ */
 public class ScrumBoardServiceImpl extends GenericServiceImpl<ScrumBoard, Long> implements ScrumBoardService {
 
 	public ScrumBoardServiceImpl(ScrumBoardDao scrumBoardDao) {
